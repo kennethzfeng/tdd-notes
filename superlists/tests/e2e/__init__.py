277fe0194990e693726931d783f5ee82c683f897
Kenneth Feng <kennethzfeng@gmail.com>
2014-08-24 21:27:53 -0400

    Added Unittest for Chapter 2

--- superlists/tests/e2e/__init__.py
+++ superlists/tests/e2e/__init__.py
@@ -5,8 +5,25 @@ Functional Tests
 """
 
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def test_can_start_a_list_and_retrive_it_later(self):
+        # Kenneth has heard about a cool new site
+        # He goes to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # He notices that the title of the page mentioned 'To-Do'
+        self.assertIn('To-Do', self.browser.title)
+
+        self.fail('Finish the test')
+
+    def tearDown(self):
+        self.browser.quit()
+
+if __name__ == "__main__":
+    unittest.main(warnings='ignore')
